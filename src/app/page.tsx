3ae8cf93746a2514171979eb740a48af9bb53511
Kadacheahmedrami <a_kadache@estin.dz>
2025-02-07 05:26:13 +0100

    landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-
 export default function Home() {
-
   return (
- <></>
+    <>
+      <div className="bg-black relative">
+        <div className="h-screen w-screen flex justify-normal items-center bg-opacity-50 bg-[#21145C] relative">
+          <div className="blur1 absolute"></div>
+          <div className="blur2 absolute"></div>
+        </div>
+        
+
+      </div>
+    </>
   );
 }
